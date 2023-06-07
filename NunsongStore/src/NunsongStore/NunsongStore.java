4f4d1d95b9a4c994d5de5564d866dc0ac46feaa0
minzix <minzikx@sookmyung.ac.kr>
2023-06-07 20:04:38 +0900

    created login panel

---
+++ NunsongStore/src/NunsongStore/NunsongStore.java
@@ -0,0 +1,11 @@
+package NunsongStore;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NunsongStore extends JPanel{
+    public NunsongStore() {
+        // 패널 구성 요소 추가
+        // ...
+    }
+}
