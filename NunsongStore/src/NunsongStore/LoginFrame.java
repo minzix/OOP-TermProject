7799087f8e4e0f96ed6bf14c692da00cbc8bb300
minzix <126869805+minzix@users.noreply.github.com>
2024-02-11 23:53:33 +0900

    Update LoginFrame.java

--- NunsongStore/src/NunsongStore/LoginFrame.java
+++ NunsongStore/src/NunsongStore/LoginFrame.java
@@ -1,19 +1,24 @@
+package NunsongStore;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 import java.sql.*;
 
 public class LoginFrame extends JFrame {
+
     private JTextField usernameField;
     private JPasswordField passwordField;
-    private Connection connection;
+    private JButton loginButton;
+    private Connection connection; // 추가함
     
     private void openNewPanel() {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("눈송상점");
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            frame.getContentPane().add(new NunsongStore());
-            frame.pack();
+            frame.getContentPane().add(new Selection());
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
             frame.setVisible(true);
         });
     }
@@ -29,8 +34,16 @@ public class LoginFrame extends JFrame {
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.insets = new Insets(10, 10, 10, 10);
 
-        ImageIcon imageIcon = new ImageIcon("noonsong.png");
-        JLabel imageLabel = new JLabel(imageIcon);
+        ImageIcon imageIcon = new ImageIcon("src/image/noonsong.png");
+        JLabel imageLabel1 = new JLabel(new ImageIcon(imageIcon.getImage().getScaledInstance(imageIcon.getIconWidth() / 5, imageIcon.getIconHeight() / 5, Image.SCALE_DEFAULT)));
+        JLabel imageLabel2 = new JLabel(new ImageIcon(imageIcon.getImage().getScaledInstance(imageIcon.getIconWidth() / 5, imageIcon.getIconHeight() / 5, Image.SCALE_DEFAULT)));
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        panel.add(imageLabel1, constraints);
+
+        constraints.gridx = 1;
+        panel.add(imageLabel2, constraints);
 
         JPanel usernamePanel = new JPanel();
         JLabel usernameLabel = new JLabel("ID: ");
@@ -43,19 +56,20 @@ public class LoginFrame extends JFrame {
         passwordField = new JPasswordField(20);
         passwordPanel.add(passwordLabel);
         passwordPanel.add(passwordField);
-        
-        connectToDatabase();
-        JButton loginButton = new JButton("Login");
+
+        loginButton = new JButton("Login");
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String username = usernameField.getText();
                 String password = new String(passwordField.getPassword());
+                dispose();
                 
-                //데이터 베이스 연결은 했으나, 컴퓨터에 MySQL이 없을 경우를 대비해 로그인 버튼을 누르면 다음 페이지로 넘어가도록 설정
+                // 데이터 베이스 연결은 했으나, 컴퓨터에 MySQL이 없을 경우를 대비해 로그인 버튼을 누르면 다음 페이지로 넘어가도록 설정
                 JOptionPane.showMessageDialog(LoginFrame.this, "Login Successful");
-                
-                //데이터 베이스 연결
+                openNewPanel();	
+                dispose();
+                // 데이터 베이스 연결
                 /*if(validateLogin(username, password)) {
                     JOptionPane.showMessageDialog(LoginFrame.this, "Login Successful");
                     openNewPanel();
@@ -75,23 +89,75 @@ public class LoginFrame extends JFrame {
         });
 
         constraints.gridx = 0;
-        constraints.gridy = 0;
-        panel.add(imageLabel, constraints);
-
         constraints.gridy = 1;
+        constraints.gridwidth = 2; // 수정된 부분
+        constraints.anchor = GridBagConstraints.CENTER; // 수정된 부분
         panel.add(usernamePanel, constraints);
-
+        
+        constraints.gridx = 0;
         constraints.gridy = 2;
         panel.add(passwordPanel, constraints);
-
+        
+        constraints.gridx = 0;
         constraints.gridy = 3;
+        constraints.gridwidth = 2; // 수정된 부분
         panel.add(loginButton, constraints);
-
+        
+        constraints.gridx = 0;
         constraints.gridy = 4;
+        constraints.gridwidth = 2; // 수정된 부분
         panel.add(signupButton, constraints);
 
+
+        getContentPane().setBackground(Color.WHITE);
         add(panel);
         pack();
+
+        // Snowflake 이미지를 움직이는 스레드 실행
+        Thread movingThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int x1 = -imageIcon.getIconWidth() / 5;
+                int x2 = getWidth() - imageIcon.getIconWidth() / 5;
+                int direction1 = 1;
+                int direction2 = -1;
+
+                Random random = new Random();
+
+                while (true) {
+                    x1 += direction1;
+                    x2 += direction2;
+
+                    if (x1 >= getWidth() - imageIcon.getIconWidth() / 5 || x1 <= -imageIcon.getIconWidth() / 5) {
+                        direction1 *= -1;
+                    }
+
+                    if (x2 >= getWidth() - imageIcon.getIconWidth() / 5 || x2 <= -imageIcon.getIconWidth() / 5) {
+                        direction2 *= -1;
+                    }
+
+                    imageLabel1.setBounds(x1, 20, imageIcon.getIconWidth() / 5, imageIcon.getIconHeight() / 5);
+                    imageLabel2.setBounds(x2, 20, imageIcon.getIconWidth() / 5, imageIcon.getIconHeight() / 5);
+
+                    if (Math.abs(x1 - x2) <= imageIcon.getIconWidth() / 5) {
+                        int red = random.nextInt(256);
+                        int green = random.nextInt(256);
+                        int blue = random.nextInt(256);
+                        loginButton.setBackground(new Color(red, green, blue));
+                    } else {
+                        loginButton.setBackground(signupButton.getBackground());
+                    }
+
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        movingThread.start();
     }
 
     private void showSignupForm() {
@@ -118,7 +184,7 @@ public class LoginFrame extends JFrame {
         signupPanel.add(emailLabel, constraints);
         signupPanel.add(emailField, constraints);
 
-        int result = JOptionPane.showConfirmDialog(LoginFrame.this, signupPanel, "Sign Up", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        int result = JOptionPane.showConfirmDialog(this, signupPanel, "Sign Up", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         if (result == JOptionPane.OK_OPTION) {
             String username = usernameField.getText();
@@ -129,11 +195,10 @@ public class LoginFrame extends JFrame {
             System.out.println("PW: " + password);
             System.out.println("Email: " + email);
 
-            JOptionPane.showMessageDialog(LoginFrame.this, "Sign Up Successful");
+            JOptionPane.showMessageDialog(this, "Sign Up Successful");
         }
     }
     
-    //database related code starts HERE 
     private boolean validateLogin(String username, String password) {
         String query = "SELECT * FROM users WHERE username = ? AND password = ?";
         try {
@@ -198,14 +263,4 @@ public class LoginFrame extends JFrame {
         super.dispose();
         disconnectFromDatabase();
     }
-    
-    
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new LoginFrame().setVisible(true);
-            }
-        });
-    }
-}
-
+} 
